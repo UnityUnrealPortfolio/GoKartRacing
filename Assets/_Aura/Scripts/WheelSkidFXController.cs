e7c181381ecb17fbe51c1e39a9eb7b2054d3e5a0
Auravision <auravision.africa@gmail.com>
2024-02-19 18:04:04 +0300

    Now have added skid trails to follow the tire when slip happens. Let's just say all this is open for polish :-)

--- Assets/_Aura/Scripts/WheelSkidFXController.cs
+++ Assets/_Aura/Scripts/WheelSkidFXController.cs
@@ -8,7 +8,14 @@ public class WheelSkidFXController : MonoBehaviour
     [SerializeField]WheelCollider[] m_wheelColliders;
     [SerializeField] AudioSource m_wheelFXAudioSource;
     [SerializeField] float m_slipThreshold;
+    [SerializeField] GameObject m_trailObject;
 
+    [SerializeField]private GameObject[] skidTrails;
+
+    private void Start()
+    {
+        skidTrails = new GameObject[4];
+    }
     private void Update()
     {
         //check for slip on each wheel collider
@@ -37,14 +44,56 @@ public class WheelSkidFXController : MonoBehaviour
                     {
                         m_wheelFXAudioSource.Play();
                     }
+
+                    //generate skid trail fx
+                    
                 }
 
             }
         }
 
+        if(slipCount > 0)
+        {
+            for(int i = 0;i< m_wheelColliders.Length;i++)
+            {
+
+                StartSkidTrail(i);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < m_wheelColliders.Length; i++)
+            {
+
+                EndSkidTrail(i);
+            }
+        }
+        
+
         if(slipCount == 0 && m_wheelFXAudioSource.isPlaying == true)
         {
             m_wheelFXAudioSource.Stop();
         }
+
+       
+    }
+
+    public void StartSkidTrail(int i)
+    {
+        if (skidTrails[i] == null)
+            skidTrails[i] = Instantiate(m_trailObject);
+
+
+        skidTrails[i].transform.parent = m_wheelColliders[i].transform;
+        skidTrails[i].transform.localPosition = -Vector3.up * m_wheelColliders[i].radius;
+        skidTrails[i].transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+    public void EndSkidTrail(int i)
+    {
+        if (skidTrails[i] == null) return;
+        Transform trailHolder = skidTrails[i].transform;
+        skidTrails[i] = null;
+        trailHolder.parent = null;
+        Destroy(trailHolder.gameObject, 30);
     }
 }
